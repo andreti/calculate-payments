4ed1cf24872f43252303d7298a561a8c6bd20cb9
Andres Santacruz <casz21@hotmail.com>
2022-07-09 22:02:26 -0500

    Initialize project and config dockerfile , actuator

---
+++ src/main/java/com/aplazo/calculateloan/CalculateloanApplication.java
@@ -0,0 +1,13 @@
+package com.aplazo.calculateloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateloanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculateloanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplazo/calculateloan/CalculateloanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplazo.calculateloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculateloanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
